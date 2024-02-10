189884689a79f62cc337d29a330a6bd603ffd7de
devinigor <devinigor@icloud.com>
2024-02-10 17:23:37 +0300

    убрал варнинги

--- UIKitLess/ViewController.swift
+++ UIKitLess/ViewController.swift
@@ -1,17 +1,16 @@
-//
-//  ViewController.swift
-//  UIKitLess
-//
-//  Created by Игорь Девин on 10.02.2024.
-//
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
 
 import UIKit
 
-class ViewController: UIViewController {
+// ViewController
 
+final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }  
-}
+        doSome()
+    }
 
+    func doSome() {}
+}
